a283c023779e441cb082cc3d63d66e44cefe43b8
Adrianna Gorsky <adriannagorsky@CarolsIPhone4S.localdomain>
2021-11-12 15:08:28 -0600

    Update with functions that were changed from my_happy_script.

--- 1_fetch/src/fetch_data.R
+++ 1_fetch/src/fetch_data.R
@@ -4,10 +4,13 @@ library(stringr)
 library(sbtools)
 library(whisker)
 
-project_output_dir <- '3_visualize/out'
+# Function to get the data from ScienceBase
+fetch_data <- function(target_name, sb_id, sb_filename) {
+  
+  item_file_download(
+    sb_id, 
+    names = sb_filename, 
+    destinations = target_name, 
+    overwrite_file = TRUE)
+}
 
-dir.create(project_output_dir)
-
-# Get the data from ScienceBase
-mendota_file <- file.path(project_output_dir, 'model_RMSEs.csv')
-item_file_download('5d925066e4b0c4f70d0d0599', names = 'me_RMSE.csv', destinations = mendota_file, overwrite_file = TRUE)
