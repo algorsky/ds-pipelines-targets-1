c19bccc8a248b434d8abfc3f040686e263a097ab
Adrianna Gorsky <adriannagorsky@Adriannas-MBP.fios-router.home>
2021-11-22 13:07:04 -0500

    update

--- 2_process/src/process_data.R
+++ 2_process/src/process_data.R
@@ -16,6 +16,7 @@ add_colors_to_data <- function(in_filepath, col_types = "iccd") {
 # Save the processed data
 save_csv<- function(data_in, project_directory, file_name){
   readr::write_csv(data_in, file = file.path(project_directory, file_name))
+  return(file_name) 
 }
 
 # Save the model diagnostics
@@ -36,5 +37,6 @@ generate_model_diagnostics<- function(data_in, project_directory, file_name){
   The PGDL prediction accuracy was more robust compared to PB when only two profiles were provided for training ({{pgdl_2mean}} and {{pb_2mean}}°C, respectively). '
   
   whisker.render(template_1 %>% str_remove_all('\n') %>% str_replace_all('  ', ' '), render_data ) %>% cat(file = file.path(project_directory, file_name))
+  return(file_name) 
 }
 
